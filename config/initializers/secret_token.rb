80499b2b889fdd578c61513596306d45db86d71b
Eric Perez <ericricardoperez@gmail.com>
2014-02-01 22:20:45 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NflApp::Application.config.secret_key_base = 'a327b1e628e21ffe33427e3542dad6c5fffa7fff721e665a9ff0af9c05b48370569c519ee0736af6dcb4872adc60cc37cd5f07ac3ba69ba0528d273aef7fa732'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NflApp::Application.config.session_store :cookie_store, key: '_nfl_app_session'
