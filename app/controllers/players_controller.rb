6540121f92bc72fa14e4087e6f91602adec036cf
Eric Perez <ericricardoperez@gmail.com>
2014-02-01 23:00:23 -0800

    find_current_player---input: player name, ouput: Nokogiri object

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,3 @@
 class PlayersController < ApplicationController
+	include PlayersHelper
 end
--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,9 @@
 module PlayersHelper
+	def find_current_player(player)
+		binding.pry
+		player = player.split.join("+")
+		url = "http://www.nfl.com/players/search?category=name&filter=#{player}&playerType=current"
+		binding.pry
+		search_page = Nokogiri::HTML(open(url))
+	end
 end
