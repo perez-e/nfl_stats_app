aa64d55312ebebe99c35018bce204d2f30a6a9e1
Eric Perez <ericricardoperez@gmail.com>
2014-02-02 20:07:20 -0800

    populating positions table with all nfl positions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+positions = ["QB", "RB", "FB","OT", "OG", "OC", "TE", "WR", "DE", "DT", "LB", "OLB", "ILB", "CB", "SS", "FS", "DB", "K", "P", "LS"]
+
+positions.each do |position|
+	Position.create(name: position)
+end
