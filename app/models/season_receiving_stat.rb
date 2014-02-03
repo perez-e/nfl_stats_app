e84864426034b2bbb7cda3a34c38f888ebdb9911
Eric Perez <ericricardoperez@gmail.com>
2014-02-03 12:41:15 -0800

    create array of SeasonReceivingStat objects based on player name

--- app/models/season_receiving_stat.rb
+++ app/models/season_receiving_stat.rb
@@ -23,4 +23,32 @@
 
 class SeasonReceivingStat < ActiveRecord::Base
   belongs_to :player
+
+  	@@element_indices = {year:0, team:1, g:2, rec:3, yds:4, avg_rec:5, yds_p_g:6, lng:7,
+  						 td:8, twenty:9, forty:10, first:11, fum:12}
+
+    def self.receiving_object(player)
+    	rec = receiving_stat(player)
+    	rec_object = []
+    	rec.each do |info|
+    		rec_object << SeasonReceivingStat.new(info)
+    	end
+    	return rec_object
+    end
+
+    private
+    
+    def self.receiving_stat(player)
+    	receiving = []
+    	stats = Player.careerstats(player, 'Receiving')
+    	stats.each do |stat|
+    		rec_hash = {}
+    		@@element_indices.each do |k, v|
+    			rec_hash[k] = stat[v]
+    		end
+    		receiving << rec_hash
+    	end
+    	return receiving
+    end
+
 end
