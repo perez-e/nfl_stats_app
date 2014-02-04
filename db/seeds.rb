738114b8d1b8bd47a7ade5ff968c8bf6645f8be6
Eric Perez <ericricardoperez@gmail.com>
2014-02-03 16:58:44 -0800

    seed michael vick for test case

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,12 @@ positions = ["QB", "RB", "FB","OT", "OG", "OC", "TE", "WR", "DE", "DT", "LB", "O
 positions.each do |position|
 	Position.create(name: position)
 end
+
+
+info = Player.player_info('Michael Vick')
+p = Player.create(info)
+
+p.season_passing_stats = SeasonPassingStat.passing_object(p.name)
+p.season_rushing_stats = SeasonRushingStat.rushing_object(p.name)
+p.season_receiving_stats = SeasonReceivingStat.receiving_object(p.name)
+p.season_defensive_stats = SeasonDefensiveStat.defensive_object(p.name)
