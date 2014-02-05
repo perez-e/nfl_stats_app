b26724b5943d8e38a29a2b6e4013d42347149284
Eric Perez <ericricardoperez@gmail.com>
2014-02-05 12:41:52 -0800

    maintain sesssion once user logs in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+    def create
+		user=User.find_by_email(params[:user][:email].downcase)
+		if user && user.authenticate(params[:user][:password])
+			# Sign the user in and redirect to the user's show page.
+			sign_in user
+			redirect_to "/"
+		else
+			# Create an error message and re-render the signin form.
+			flash.now[:error]='Invalid email/password combination'
+			redirect_to "/"
+		end
+    end
+
+    def destroy
+        sign_out
+        redirect_to root_url
+    end
 end
