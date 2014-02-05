50ffe15e6a08d464c5c6fb4b168d893f3d60587d
Eric Perez <ericricardoperez@gmail.com>
2014-02-04 23:16:03 -0800

    remember token for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,12 @@
 
 class User < ActiveRecord::Base
 	has_secure_password
+
+	before_save :create_remember_token
+
+    private
+
+    def create_remember_token
+        self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
