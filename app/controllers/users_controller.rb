0218bdc7292ca6ce145d1ce03e0d4d177a8b8975
Eric Perez <ericricardoperez@gmail.com>
2014-02-04 23:29:25 -0800

    able to create user from sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+	def create
+		user = params[:user].permit(:first_name, :last_name, :email, :password, :password_confirmation)
+		User.create(user)
+
+		redirect_to "/"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ NflApp::Application.routes.draw do
 	root to: "players#index"
     get "players/:id", to: "players#show"
     post "/players", to: "players#create"
+    post "/users", to: "users#create"
 end
