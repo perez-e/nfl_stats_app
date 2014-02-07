d8363f39407b1d5679ba6275aacd6cde8f830c33
Eric Perez <ericricardoperez@gmail.com>
2014-02-06 23:26:36 -0800

    add validations for user on create

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,21 @@ class User < ActiveRecord::Base
 	has_many :players, through: :user_players
 	before_save :create_remember_token
 
+	before_save do |user| 
+		user.email = email.downcase
+		user.first_name = first_name.capitalize
+		user.last_name = last_name.capitalize
+	end 
+
+	validates :first_name, presence: true, length: { maximum: 50 }
+	validates :last_name, presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence:   true,
+	                format:     { with: VALID_EMAIL_REGEX },
+	                uniqueness: { case_sensitive: false }
+	validates :password, presence: true, length: { minimum: 6 }, on: :create
+	validates :password_confirmation, presence: true, on: :create
+
     private
 
     def create_remember_token
