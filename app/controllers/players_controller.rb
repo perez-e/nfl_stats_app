9c04ace3538c086001b0ab79e2950fbf5e128a3a
Eric Perez <ericricardoperez@gmail.com>
2014-04-20 21:06:58 -0700

    delete the create method in players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -11,30 +11,6 @@ class PlayersController < ApplicationController
 		gon.player_defensive = @player.season_defensive_stats.order(year: :asc)
 	end
 
-	def create
-		player_name = params[:player][:name]
-		player_info = Player.player_info(player_name)
-
-		if !player_info.blank? && !Player.find_by_nfl_id(player_info[:nfl_id])
-			# if player is not in database create player and fill in stats
-			p = Player.create(player_info)
-			p.season_passing_stats = SeasonPassingStat.passing_object(p.name)
-			p.season_rushing_stats = SeasonRushingStat.rushing_object(p.name)
-			p.season_receiving_stats = SeasonReceivingStat.receiving_object(p.name)
-			p.season_defensive_stats = SeasonDefensiveStat.defensive_object(p.name)
-
-			redirect_to "/players/#{p.nfl_id}"
-
-		elsif !player_info.blank? && Player.find_by_nfl_id(player_info[:nfl_id])
-			# if player is in the database pull up there stats
-			p = Player.find_by_nfl_id(player_info[:nfl_id])
-			redirect_to "/players/#{p.nfl_id}"
-
-		else 
-			redirect_to root_path
-		end
-	end
-
 	def favorite 
 		p = Player.find_by_nfl_id(params[:nfl_id])
 		user = current_user
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ NflApp::Application.routes.draw do
     get "/players/:nfl_id", to: "players#show"
     patch "/players/:nfl_id", to: "players#favorite"
     delete "/players/:nfl_id", to: "players#remove"
-    post "/players", to: "players#create"
     post "/users", to: "users#create"
     delete "/users/:id", to: "users#destroy"
     post "/sessions", to: "sessions#create"
